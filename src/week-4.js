0cd7e25361b53ae3c4b1bfbbdcc3d378c6555191
Max Squire <maxbriansquire@gmail.com>
2022-03-16 11:01:48 +1300

    java

--- src/week-4.js
+++ src/week-4.js
@@ -2,7 +2,38 @@
 /*
 javascript variables
 */
-var name ="Max Squire";
+var fullName ="Max Squire";
 var classcode = 503
 console.log(fullName)
 console.log(classcode)
+
+// javascript objects
+var full_name = 
+{
+first_name: "Max",
+last_name: "Squire",
+course_number:503
+}
+console.log(full_name)
+console.log(full_name.first_name)
+console.log(full_name["last_name"])
+
+//javascript functions
+function addTwoNumbers(num1, num2) {
+    return num1+num2
+}
+console.log(addTwoNumbers(12,3))
+
+function addThreeNumbers(num1, num2, num3) {
+
+    return num1+num2+num3
+    console.log(addThreeNumbers(12,15,30))
+
+}
+
+//javascript loops
+
+for(var i=0; i< 10; i++){
+    console.log(i)
+    
+}
